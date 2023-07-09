eaad727eafa0348772908e240d2d7d1032cfa186
rakeshb <rakeshb@cloudera.com>
2023-07-09 22:16:35 +0530

    Override default admin page layout

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important Dates'), {'fields': ('last_login')}),
+    )
+    readonly_fields = ['last_login']
+
+
+admin.site.register(User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -6,13 +6,23 @@ from django.contrib.auth.models import (
 )
 
 
-def UserManager(BaseUserManager):
+class UserManager(BaseUserManager):
     """Manager for users"""
 
-    def create_user(self, email, password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, Save and return a new user."""
-        user = self.model(email=email, **extra_field)
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
@@ -26,4 +36,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = email
+    USERNAME_FIELD = 'email'
