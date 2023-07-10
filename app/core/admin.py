6f15d858fdcbadf241b21460e9c2c237e93dd028
rakeshb <rakeshb@cloudera.com>
2023-07-10 10:33:23 +0530

    Add field sets to modify create user interface

--- app/core/admin.py
+++ app/core/admin.py
@@ -24,9 +24,24 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ),
-        (_('Important Dates'), {'fields': ('last_login')}),
+        (_('Important Dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(User, UserAdmin)
